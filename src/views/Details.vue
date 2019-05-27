7513d53a625f78566259d32e4213974dd9e7516d
Akira <meyer.jeffrey@hotmail.nl>
2019-05-27 13:27:55 +0200

    Generalize files
    Added files and edited files to prepare them for their functionality

---
+++ src/views/Details.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="detailpage">
+    <h1>This is the detail page</h1>
+    <!-- Import Detailpage functionality below -->
+  </div>
+</template>
---
+++ src/views/Randomizer.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="randomizer">
+        <h1>Randomizer Page</h1>
+        <!-- Import component randomizer below -->
+    </div>
+</template>
